3b14eb5366e1b43a832744711f276d5ccbf34e46
wanpc4 <idrissilhan@gmail.com>
2024-08-05 17:05:23 +0800

    add Libft folder

---
+++ Libft/libft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wwan-ab- <wwan-ab-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/04 10:27:37 by wwan-ab-          #+#    #+#             */
+/*   Updated: 2024/06/25 23:19:09 by wwan-ab-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+#define LIBFT_H
+#include <stdlib.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <limits.h>
+char	*ft_itoa(int n);
+void	ft_putchar_fd(char c, int fd);
+#endif
